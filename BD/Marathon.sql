4d85ecab3522dbe331d0b38004db9fc5c9f28a2f
christopher <christopher.bourrel0429@gmail.com>
2020-03-04 08:05:57 +0100

    Update DB

--- BD/Marathon.sql
+++ BD/Marathon.sql
@@ -1,4 +1,5 @@
 
+DROP DATABASE marathon;
 CREATE DATABASE IF NOT EXISTS marathon DEFAULT CHARACTER SET utf8;
 USE marathon;
 
@@ -18,6 +19,7 @@ create table T_USER (
   U_ID integer primary key auto_increment,
   U_PASSWORD varchar(10) not null,
   U_NATIONALITE varchar(3),
+  U_ADMIN boolean default false,
 	CONSTRAINT FK_NATIONALITE foreign key(U_NATIONALITE) references T_NATIONALITE(N_NATIONALITE)
 );
 
@@ -40,3 +42,21 @@ create table T_PATICIPANT (
   CONSTRAINT FK_ID_COURSE foreign key(P_ID_COURSE) references T_COURSE(C_ID_COURSE),
   CONSTRAINT FK_PARTICIPANT foreign key(P_PARTICIPANT) references T_USER(U_ID)
 );
+
+INSERT INTO T_NATIONALITE(N_NATIONALITE) values ('BEL');
+INSERT INTO T_NATIONALITE(N_NATIONALITE) values ('FRA');
+INSERT INTO T_NATIONALITE(N_NATIONALITE) values ('ENG');
+
+INSERT INTO T_USER (U_NOM,U_PASSWORD,U_NATIONALITE,U_ADMIN)
+VALUES ('user','user','BEL','0');
+INSERT INTO T_USER (U_NOM,U_PASSWORD,U_NATIONALITE,U_ADMIN)
+VALUES ('admin','admin','BEL','1');
+
+INSERT INTO T_REGION
+VALUES ('Bruxelle'),
+        ('Mons'),
+        ('Londre'),
+        ('Liverpool'),
+        ('Paris'),
+        ('Lyon');
+
