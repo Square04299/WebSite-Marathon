f31227414896b798a93f2b66e3f191b8836be076
christopher <christopher.bourrel0429@gmail.com>
2020-03-06 11:29:24 +0100

    Page admin creation form

--- view/Admin/index.php
+++ view/Admin/index.php
@@ -7,30 +7,35 @@ Ce blog comporte <?= $this->clean($nbPosts) ?> billet(s) et <?= $this->clean($nb
 <br>
 <a id="decoLink" href="connection/disconnect">Se déconnecter</a>
 -->
-<link rel="stylesheet" href="Content/connection.css" />
+
 
 <!-- Need to create form for course -->
-<div class="create">
-    <h1>Create new Marathon</h1>
-    <form method="post" action="Admin">
-        <p>
-            <label>Nom de la course : </label>
+<div class="container">
+    <div class="box">
+        <h1>Create new Marathon</h1>
+        <form method="post" action="Admin">
+            <label>Nom de la course</label>
                 <input type="text" name="nom" value="" placeholder="Nom de la course">
-            <label>Date de la course : </label>
-                <input type="text" name="date" value="" placeholder="Date">
-        </p>
-        <p>
+            <label>Date de la course</label>
+                <input type="date" name="date" value="" placeholder="Date">
+
             <label>Lieux de départ </label>
                 <input type="text" name="lieuxD" value="" placeholder=""> <!-- change dropdown -->
             <label>Lieux d'arriver </label>
                 <input type="text" name="lieuxA" value="" placeholder=""><!-- change dropdown -->
-
-        <p>
-            <input type="text" name="temps" value="" placeholder=""> <!-- -->
-            <input type="text" name="distance" value="" placeholder=""> <!-- -->
-        </p>
-        <p class="submit">
-            <input type="submit" name="commit" value="Create Marathon">
-        </p>
-    </form>
-  </div>
+            <label>Temps</label>
+                <input type="text" name="temps" value="" placeholder=""> <!-- -->
+            <label>Distance</label>
+                <input type="text" name="distance" value="" placeholder=""> <!-- -->
+            <div>
+                <input class="reset" type="reset" name="reset" value="Reset">
+                <input class="submit" type="submit" name="commit" value="Create">
+            </div>
+        </form>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <h1>Marathon Précédent</h1>
+    </div>
+</div>
--- view/Connection/index.php
+++ view/Connection/index.php
@@ -1,25 +1,28 @@
 <?php $this->title = "Connexion" ?>
 
-<link rel="stylesheet" href="Content/connection.css" />
 <div class="info">
-  <ul>
-    <li>Username : user</li>
-    <li>Password : user</li>
-  </ul>
-  <ul>
-    <li>Username : admin</li>
-    <li>Password : admin</li>
-  </ul>
+    <ul>
+        <li>Username : user</li>
+        <li>Password : user</li>
+    </ul>
+    <ul>
+        <li>Username : admin</li>
+        <li>Password : admin</li>
+    </ul>
 </div>
 <div class="container">
-  <div class="login">
-    <h1>Connection Marathon</h1>
-    <form method="post" action="connection/connect">
-      <p><input type="text" name="login" value="" placeholder="Username"></p>
-      <p><input type="password" name="password" value="" placeholder="Password"></p>
-      <p class="submit"><input type="submit" name="commit" value="Login"></p>
-    </form>
-  </div>
+    <div class="box">
+        <h1>Connection Marathon</h1>
+        <form method="post" action="connection/connect">
+            <div>
+                <input type="text" name="login" value="" placeholder="Username">
+            </div>
+            <div>
+                <input type="password" name="password" value="" placeholder="Password">
+            </div>
+            <div class="submit"><input type="submit" name="commit" value="Login"></div>
+        </form>
+    </div>
 </div>
 
 <?php if (isset($msgError)): ?>
